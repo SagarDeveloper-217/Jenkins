ed0b32f0994b9822287fbca9b53a9d75c636e1e0
Sagar Pilla <sagarluckyguy217@gmail.com>
2024-06-19 14:24:06 +0530

    third commit

--- src/test/java/Spring_Jenkins/JenkinsApplicationTests.java
+++ src/test/java/Spring_Jenkins/JenkinsApplicationTests.java
@@ -4,21 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class JenkinsApplicationTests {
 	
-	public static org.slf4j.Logger Logger=LoggerFactory.getLogger(JenkinsApplicationTests.class);
+	public static Logger Logger=LoggerFactory.getLogger(JenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
 		Logger.info("Test Case executing ...");
 		assertEquals(true, true);
 	}
-
-	
 
 }
